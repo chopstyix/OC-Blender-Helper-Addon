6fb5f19f1fccc757272012fbcae77491966ef2c5
Yichen-Dou <yichen.dou@outlook.com>
2020-06-16 01:14:01 -0400

    Fixed converstion between Universal and others

--- Octane_Helper/operators/nodes.py
+++ Octane_Helper/operators/nodes.py
@@ -316,10 +316,18 @@ class OctaneNodeConvertTo(Operator):
             newNode.location = active_node.location
 
             # A list of socket objects, not strs
-            common_sockets = [in_socket for in_socket in active_node.inputs if in_socket.name in newNode.inputs]
+            # Different names for Diffuse color in Universal and Other shaders. We need to handle it. This shoule be removed in the future
+            is_universal_to_other = (active_node.bl_idname == 'ShaderNodeOctUniversalMat' and newNode.bl_idname != 'ShaderNodeOctUniversalMat' and 'Mat' in newNode.bl_idname)
+            is_other_to_universal = ('Mat' in active_node.bl_idname and newNode.bl_idname == 'ShaderNodeOctUniversalMat' and active_node.bl_idname != 'ShaderNodeOctUniversalMat')
+            common_sockets = [in_socket for in_socket in active_node.inputs if (in_socket.name in newNode.inputs) or (in_socket.name in ['Albedo color', 'Diffuse'] and ('Albedo color' in newNode.inputs or 'Diffuse' in newNode.inputs))]
             for common_socket in common_sockets:
                 if(common_socket.is_linked):
-                    ntree.links.new(common_socket.links[0].from_node.outputs[0], newNode.inputs[common_socket.name])
+                    if(common_socket.name == 'Albedo color' and is_universal_to_other):
+                        ntree.links.new(common_socket.links[0].from_node.outputs[0], newNode.inputs['Diffuse'])
+                    elif(common_socket.name == 'Diffuse' and is_other_to_universal):
+                        ntree.links.new(common_socket.links[0].from_node.outputs[0], newNode.inputs['Albedo color'])
+                    else:
+                        ntree.links.new(common_socket.links[0].from_node.outputs[0], newNode.inputs[common_socket.name])
                 elif(hasattr(common_socket, 'default_value') and common_socket.name not in except_list):
                     newNode.inputs[common_socket.name].default_value = common_socket.default_value
 
